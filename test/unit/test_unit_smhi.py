536e8c87d8f8cacba3a81931aa2322739d8cbefa
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-09-25 23:54:29 +0200

    Update unit test for SMHIClient

--- test/unit/test_unit_smhi.py
+++ test/unit/test_unit_smhi.py
@@ -3,10 +3,10 @@ SMHI unit tests.
 """
 
 from unittest.mock import patch
-from smhi.smhi import SMHI
+from smhi.smhi import SMHIClient
 
 
-class TestUnitSMHI:
+class TestUnitSMHIClient:
     """
     Unit tests for SMHI class.
     """
@@ -15,14 +15,14 @@ class TestUnitSMHI:
     @patch("smhi.smhi.json.loads")
     def test_unit_smhi_init(self, mock_requests_get, mock_json_loads):
         """
-        Unit test for SMHI init method.
+        Unit test for SMHIClient init method.
 
         Args:
             mock_requests_get: mock requests get method
             mock_json_loads: mock json loads method
         """
-        smhi = SMHI()
+        client = SMHIClient()
 
-        assert smhi.type == "application/json"
+        assert client.type == "application/json"
         mock_requests_get.assert_called_once()
         mock_json_loads.assert_called_once()
