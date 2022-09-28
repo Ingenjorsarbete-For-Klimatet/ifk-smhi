729e1a8b76b2bf69f442ad7851bb98bb105c2cc6
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-09-28 23:19:02 +0200

    One unit test missing, for fetch_and_load_strang_data

--- test/unit/test_unit_strang.py
+++ test/unit/test_unit_strang.py
@@ -1,8 +1,9 @@
 """
 SMHI unit tests.
 """
+import pytest
 from unittest.mock import patch
-from smhi.strang import Strang
+from smhi.strang import Strang, check_date_validity, fetch_and_load_strang_data
 from functools import partial
 from smhi.constants import (
     STRANG,
@@ -73,10 +74,26 @@ class TestUnitStrang:
             is True
         )
 
+    @pytest.mark.parametrize(
+        "lon, lat, parameter, time_from, time_to, time_interval",
+        [
+            (0, 0, 0, "2020-01-01", "2020-01-01", "hourly"),
+            (116, 0, 0, None, None, None),
+            (116, 0, 0, "2020-01-01", "2020-01-01", "hourly"),
+        ],
+    )
     @patch("smhi.strang.check_date_validity")
     @patch("smhi.strang.fetch_and_load_strang_data")
     def test_unit_strang_fetch_data(
-        self, mock_check_date_validity, mock_fetch_and_load_strang_data
+        self,
+        mock_check_date_validity,
+        mock_fetch_and_load_strang_data,
+        lon,
+        lat,
+        parameter,
+        time_from,
+        time_to,
+        time_interval,
     ):
         """
         Unit test for STRÅNG fetch_data method.
@@ -84,15 +101,135 @@ class TestUnitStrang:
         Args:
             mock_check_date_validity: mock check_date_validity method
             mock_fetch_and_load_strang_data: mock fetch_and_load_strang_data method
+            lon: longitude
+            lat: latitude
+            parameter: parameter
+            time_from: from time
+            time_to: to time
+            time_interval: time interval
         """
         client = Strang()
-        pass
+        url = STRANG_URL
+        time_url = STRANG_URL_TIME
+
+        if parameter == 0:
+            with pytest.raises(NotImplementedError):
+                client.fetch_data(
+                    lon,
+                    lat,
+                    parameter,
+                    time_from,
+                    time_to,
+                    time_interval,
+                )
+        else:
+            client.fetch_data(
+                lon,
+                lat,
+                parameter,
+                time_from,
+                time_to,
+                time_interval,
+            )
+
+            assert client.longitude == lon
+            assert client.latitude == lat
+            assert (
+                client.parameter
+                == [p for p in self.available_parameters if p.parameter == parameter][0]
+            )
+
+            url_dict = {"lon": lon, "lat": lat, "parameter": parameter.parameter}
+            for k1, k2 in zip(
+                sorted(url_dict.keys()), sorted(client.url.keywords.keys())
+            ):
+                assert k1 == k2
+                assert url_dict[k1] == client.url.keywords[k2]
+
+            if time_from is not None:
+                mock_check_date_validity.assert_called_once_with(
+                    parameter,
+                    url,
+                    time_url,
+                    time_from,
+                    time_to,
+                    time_interval,
+                )
+            mock_fetch_and_load_strang_data.assert_called_once_with(url)
 
     def test_unit_strang_check_date_validity(self):
         """
-        Unit test for STRANG check_date_validity function.
+        Unit test for STRANG check_date_validity function, type error and interval error.
         """
-        pass
+        parameter = STRANG_PARAMETERS[0]
+        url = "URL"
+        time_url = "URL_TIME"
+
+        with pytest.raises(TypeError):
+            time_from = "2020-01-01"
+            time_to = None
+            time_interval = "hourly"
+            check_date_validity(
+                parameter, url, time_url, time_from, time_to, time_interval
+            )
+
+        with pytest.raises(ValueError):
+            time_from = "2020-01-01"
+            time_to = "2022-01-01"
+            time_interval = "minutly"
+            check_date_validity(
+                parameter, url, time_url, time_from, time_to, time_interval
+            )
+
+    @pytest.mark.parametrize(
+        "time_from, time_to",
+        [
+            ("20222-01-01", "2022-01-01"),
+            ("2022-012-01", "2022-01-01"),
+            ("2022-01-012", "2022-01-01"),
+            ("2022-01-01", "20222-01-01"),
+            ("2022-01-01", "2022-012-01"),
+            ("2022-01-01", "2022-01-012"),
+            ("1900-01-01", "2022-01-01"),
+            ("2010-01-01", "3000-01-01"),
+            ("2010-01-01", "1900-01-01"),
+            ("3000-01-01", "2022-01-01"),
+        ],
+    )
+    def test_unit_strang_check_date_validity_valueerror(self, time_from, time_to):
+        """
+        Unit test for STRANG check_date_validity function, value error, bounds and format.
+
+        Args:
+            time_from: time from
+            time_to: time to
+        """
+        parameter = STRANG_PARAMETERS[0]
+        url = "URL"
+        time_url = "URL_TIME"
+        time_interval = "hourly"
+
+        with pytest.raises(ValueError):
+            check_date_validity(
+                parameter, url, time_url, time_from, time_to, time_interval
+            )
+
+    def test_unit_strang_check_date_validity_correct(self):
+        """
+        Unit test for STRANG check_date_validity function, correct input.
+        """
+        parameter = STRANG_PARAMETERS[0]
+        url = "URL"
+        time_url = "{time_from} {time_to} {time_interval}"
+        time_from = "2020-01-01"
+        time_to = "2020-01-02"
+        time_interval = "hourly"
+
+        returned_url = check_date_validity(
+            parameter, url, time_url, time_from, time_to, time_interval
+        )
+
+        assert returned_url == "URL2020-01-01 2020-01-02 hourly"
 
     @patch("smhi.strang.requests.get")
     @patch("smhi.strang.json.loads")
@@ -106,4 +243,7 @@ class TestUnitStrang:
             mock_requests_get: mock requests get method
             mock_json_loads: mock json loads method
         """
+        # url = "URL"
+        # fetch_and_load_strang_data(url)
+        # mock_requests_get.assert_called_once_with(url)
         pass
