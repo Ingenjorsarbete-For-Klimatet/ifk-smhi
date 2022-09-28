1d4ab393c21be6a824e03e072a63c5da4c2d7efb
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-09-28 19:21:42 +0200

    Split into smaller functions and simplify

--- smhi/strang.py
+++ smhi/strang.py
@@ -6,6 +6,7 @@ import requests
 from datetime import datetime
 from functools import partial
 from smhi.constants import (
+    STRANG,
     STRANG_URL,
     STRANG_URL_TIME,
     STRANG_PARAMETERS,
@@ -13,6 +14,78 @@ from smhi.constants import (
     STRANG_DATETIME_FORMAT,
     STRANG_TIME_INTERVALS,
 )
+
+
+def check_date_validity(
+    parameter: STRANG,
+    url: str,
+    time_url: str,
+    time_from: str,
+    time_to: str,
+    time_interval: str,
+):
+    """
+    Check validity of input dates
+
+    Args:
+        parameter: selected parameter
+        url:
+        time_url:
+        time_from:
+        time_to:
+        time_interval:
+    """
+    if time_from is not None:
+        if time_from is None:
+            raise NotImplementedError("All time arguments must be set.")
+
+        time_now = datetime.now()
+
+        try:
+            time_from_parsed = datetime.strptime(time_from, STRANG_DATE_FORMAT)
+            time_to_parsed = datetime.strptime(time_to, STRANG_DATE_FORMAT)
+        except ValueError:
+            raise ValueError("Wrong format of from date, use %Y-%m-%d.")
+
+        if (
+            time_from_parsed < parameter.time_from
+            or time_to_parsed < parameter.time_from
+        ):
+            raise ValueError("Data does not exist that far back.")
+        if time_from_parsed > time_now or time_to_parsed > time_now:
+            raise ValueError("Data does not exist for the future.")
+
+        if time_interval not in STRANG_TIME_INTERVALS:
+            raise ValueError("Time interval must be hourly, daily, monthly.")
+
+        url = url + time_url.format(
+            time_from=time_from,
+            time_to=time_to,
+            time_interval=time_interval,
+        )
+
+        return url
+
+
+def fetch_and_load_strang_data(url: str):
+    """
+    Fetch requested data and parse it with datetime.
+
+    Args:
+        url: url to fetch data
+    """
+    response = requests.get(url)
+    status = response.ok
+    headers = response.headers
+    if status is True:
+        data = json.loads(response.content)
+
+        for entry in self.data:
+            entry["date_time"] = datetime.strptime(
+                entry["date_time"], STRANG_DATETIME_FORMAT
+            )
+
+    return status, headers, data
 
 
 class Strang:
@@ -82,48 +155,13 @@ class Strang:
             lat=self.latitude,
             parameter=self.parameter.parameter,
         )
-        if time_from is not None:
-            if time_from is None:
-                raise NotImplementedError("All time arguments must be set.")
 
-            time_now = datetime.now()
-
-            try:
-                time_from_parsed = datetime.strptime(time_from, STRANG_DATE_FORMAT)
-            except ValueError:
-                raise ValueError("Wrong format of from date, use %Y-%m-%d.")
-
-            if time_from_parsed < self.parameter.time_from:
-                raise ValueError("Data does not exist that far back.")
-            if time_from_parsed > time_now:
-                raise ValueError("Data does not exist for the future.")
-
-            try:
-                time_to_parsed = datetime.strptime(time_to, STRANG_DATE_FORMAT)
-            except ValueError:
-                raise ValueError("Wrong format of to date, use %Y-%m-%d.")
-
-            if time_to_parsed < self.parameter.time_from:
-                raise ValueError("Data does not exist that far back.")
-            if time_to_parsed > time_now:
-                raise ValueError("Data does not exist for the future.")
-
-            if time_interval not in STRANG_TIME_INTERVALS:
-                raise ValueError("Time interval must be hourly, daily, monthly.")
-
-            self.url = self.url + self.time_url.format(
-                time_from=time_from,
-                time_to=time_to,
-                time_interval=time_interval,
-            )
-
-        response = requests.get(self.url)
-        self.status = response.ok
-        self.headers = response.headers
-        if self.status is True:
-            self.data = json.loads(response.content)
-
-            for entry in self.data:
-                entry["date_time"] = datetime.strptime(
-                    entry["date_time"], STRANG_DATETIME_FORMAT
-                )
+        self.url = check_date_validity(
+            self.parameter,
+            self.url,
+            self.time_url,
+            time_from,
+            time_to,
+            time_interval,
+        )
+        self.status, self.headers, self.data = fetch_and_load_strang_data(self.url)
