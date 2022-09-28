c7d6c82f76ecddaaa6fc68679f36bd4792a487b7
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-09-28 19:09:21 +0200

    Parse datetime data and use validity checks on request

--- smhi/strang.py
+++ smhi/strang.py
@@ -3,8 +3,16 @@ SMHI STRÅNG client.
 """
 import json
 import requests
+from datetime import datetime
 from functools import partial
-from smhi.constants import STRANG_URL, STRANG_URL_TIME, STRANG_PARAMETERS
+from smhi.constants import (
+    STRANG_URL,
+    STRANG_URL_TIME,
+    STRANG_PARAMETERS,
+    STRANG_DATE_FORMAT,
+    STRANG_DATETIME_FORMAT,
+    STRANG_TIME_INTERVALS,
+)
 
 
 class Strang:
@@ -22,6 +30,8 @@ class Strang:
         self.latitude = None
         self.longitude = None
         self.parameter = None
+        self.status = None
+        self.header = None
         self.data = None
 
         self.available_parameters = STRANG_PARAMETERS
@@ -58,7 +68,7 @@ class Strang:
         self.latitude = latitude
         self.longitude = longitude
         self.parameter = [
-            p.parameter for p in self.available_parameters if p.parameter == parameter
+            p for p in self.available_parameters if p.parameter == parameter
         ]
         if len(self.parameter) != 0:
             self.parameter = self.parameter[0]
@@ -70,11 +80,36 @@ class Strang:
         self.url = self.raw_url(
             lon=self.longitude,
             lat=self.latitude,
-            parameter=self.parameter,
+            parameter=self.parameter.parameter,
         )
         if time_from is not None:
-            if time_from is None or time_interval is None:
+            if time_from is None:
                 raise NotImplementedError("All time arguments must be set.")
+
+            time_now = datetime.now()
+
+            try:
+                time_from_parsed = datetime.strptime(time_from, STRANG_DATE_FORMAT)
+            except ValueError:
+                raise ValueError("Wrong format of from date, use %Y-%m-%d.")
+
+            if time_from_parsed < self.parameter.time_from:
+                raise ValueError("Data does not exist that far back.")
+            if time_from_parsed > time_now:
+                raise ValueError("Data does not exist for the future.")
+
+            try:
+                time_to_parsed = datetime.strptime(time_to, STRANG_DATE_FORMAT)
+            except ValueError:
+                raise ValueError("Wrong format of to date, use %Y-%m-%d.")
+
+            if time_to_parsed < self.parameter.time_from:
+                raise ValueError("Data does not exist that far back.")
+            if time_to_parsed > time_now:
+                raise ValueError("Data does not exist for the future.")
+
+            if time_interval not in STRANG_TIME_INTERVALS:
+                raise ValueError("Time interval must be hourly, daily, monthly.")
 
             self.url = self.url + self.time_url.format(
                 time_from=time_from,
@@ -83,5 +118,12 @@ class Strang:
             )
 
         response = requests.get(self.url)
+        self.status = response.ok
         self.headers = response.headers
-        self.data = json.loads(response.content)
+        if self.status is True:
+            self.data = json.loads(response.content)
+
+            for entry in self.data:
+                entry["date_time"] = datetime.strptime(
+                    entry["date_time"], STRANG_DATETIME_FORMAT
+                )
