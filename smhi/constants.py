4f08ec61c96f90f6a192c4ec4bee746030181a8c
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-09-29 22:39:09 +0200

    Simplified structure, testes failing

--- smhi/constants.py
+++ smhi/constants.py
@@ -5,11 +5,9 @@ from datetime import datetime
 from collections import defaultdict
 from collections import namedtuple
 
-
 TYPE_MAP = defaultdict(lambda: "application/json", json="application/json")
 
 METOBS_URL = "https://opendata-download-metobs.smhi.se/api.json"
-
 
 STRANG_POINT_URL = (
     "https://opendata-download-metanalys.smhi.se/api/category/"
@@ -21,43 +19,53 @@ STRANG_POINT_URL_TIME = "?from={time_from}&to={time_to}&interval={time_interval}
 
 STRANG_DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 STRANG_DATE_FORMAT = "%Y-%m-%d"
-STRANG_TIME_INTERVALS = ["hourly", "daily", "monthly"]
+STRANG_DATE_INTERVALS = ["hourly", "daily", "monthly"]
 STRANG = namedtuple("STRANG", "parameter meaning time_from time_to")
-STRANG_PARAMETERS = [
+get_now = lambda: datetime.utcnow().isoformat("T", "seconds") + "Z"
+STRANG_PARAMETERS = defaultdict(lambda: STRANG(None, "Missing", None, None))
+STRANG_PARAMETERS[116] = (
     STRANG(
         116,
         "CIE UV irradiance [mW/m²]",
         datetime.strptime("1999-01-01", STRANG_DATE_FORMAT),
-        lambda: datetime.now(),
+        get_now,
     ),
+)
+STRANG_PARAMETERS[117] = (
     STRANG(
         117,
         "Global irradiance [W/m²]",
         datetime.strptime("1999-01-01", STRANG_DATE_FORMAT),
-        lambda: datetime.now(),
+        get_now,
     ),
+)
+STRANG_PARAMETERS[118] = (
     STRANG(
         118,
         "Direct normal irradiance [W/m²]",
         datetime.strptime("1999-01-01", STRANG_DATE_FORMAT),
-        lambda: datetime.now(),
+        get_now,
     ),
+)
+STRANG_PARAMETERS[120] = (
     STRANG(
         120,
         "PAR [W/m²]",
         datetime.strptime("1999-01-01", STRANG_DATE_FORMAT),
-        lambda: datetime.now(),
+        get_now,
     ),
+)
+STRANG_PARAMETERS[121] = (
     STRANG(
         121,
         "Direct horizontal irradiance [W/m²]",
         datetime.strptime("2017-04-18", STRANG_DATE_FORMAT),
-        lambda: datetime.now(),
+        get_now,
     ),
-    STRANG(
-        122,
-        "Diffuse irradiance [W/m²]",
-        datetime.strptime("2017-04-18", STRANG_DATE_FORMAT),
-        lambda: datetime.now(),
-    ),
-]
+)
+STRANG_PARAMETERS[122] = STRANG(
+    122,
+    "Diffuse irradiance [W/m²]",
+    datetime.strptime("2017-04-18", STRANG_DATE_FORMAT),
+    get_now,
+)
