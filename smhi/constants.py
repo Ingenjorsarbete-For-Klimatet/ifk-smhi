8f6cb132e9199d98da12a1f532d6ec4d91c7172c
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-09-29 23:25:55 +0200

    Behaviour aligned better with API

--- smhi/constants.py
+++ smhi/constants.py
@@ -14,58 +14,47 @@ STRANG_POINT_URL = (
     + "{category}/version/{version}/geotype/point/lon/{lon}/lat/"
     + "{lat}/parameter/{parameter}/data.json"
 )
-STRANG_POINT_URL_TIME = "?from={time_from}&to={time_to}&interval={time_interval}"
-
 
 STRANG_DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 STRANG_DATE_FORMAT = "%Y-%m-%d"
 STRANG_DATE_INTERVALS = ["hourly", "daily", "monthly"]
-STRANG = namedtuple("STRANG", "parameter meaning time_from time_to")
-get_now = lambda: datetime.utcnow().isoformat("T", "seconds") + "Z"
+STRANG = namedtuple("STRANG", "parameter meaning date_from date_to")
+STRANG_GET_NOW = lambda: datetime.utcnow()  # .isoformat("T", "seconds") + "Z"
 STRANG_PARAMETERS = defaultdict(lambda: STRANG(None, "Missing", None, None))
-STRANG_PARAMETERS[116] = (
-    STRANG(
-        116,
-        "CIE UV irradiance [mW/m²]",
-        datetime.strptime("1999-01-01", STRANG_DATE_FORMAT),
-        get_now,
-    ),
+STRANG_PARAMETERS[116] = STRANG(
+    116,
+    "CIE UV irradiance [mW/m²]",
+    datetime.strptime("1999-01-01", STRANG_DATE_FORMAT),
+    STRANG_GET_NOW,
 )
-STRANG_PARAMETERS[117] = (
-    STRANG(
-        117,
-        "Global irradiance [W/m²]",
-        datetime.strptime("1999-01-01", STRANG_DATE_FORMAT),
-        get_now,
-    ),
+
+STRANG_PARAMETERS[117] = STRANG(
+    117,
+    "Global irradiance [W/m²]",
+    datetime.strptime("1999-01-01", STRANG_DATE_FORMAT),
+    STRANG_GET_NOW,
 )
-STRANG_PARAMETERS[118] = (
-    STRANG(
-        118,
-        "Direct normal irradiance [W/m²]",
-        datetime.strptime("1999-01-01", STRANG_DATE_FORMAT),
-        get_now,
-    ),
+STRANG_PARAMETERS[118] = STRANG(
+    118,
+    "Direct normal irradiance [W/m²]",
+    datetime.strptime("1999-01-01", STRANG_DATE_FORMAT),
+    STRANG_GET_NOW,
 )
-STRANG_PARAMETERS[120] = (
-    STRANG(
-        120,
-        "PAR [W/m²]",
-        datetime.strptime("1999-01-01", STRANG_DATE_FORMAT),
-        get_now,
-    ),
+STRANG_PARAMETERS[120] = STRANG(
+    120,
+    "PAR [W/m²]",
+    datetime.strptime("1999-01-01", STRANG_DATE_FORMAT),
+    STRANG_GET_NOW,
 )
-STRANG_PARAMETERS[121] = (
-    STRANG(
-        121,
-        "Direct horizontal irradiance [W/m²]",
-        datetime.strptime("2017-04-18", STRANG_DATE_FORMAT),
-        get_now,
-    ),
+STRANG_PARAMETERS[121] = STRANG(
+    121,
+    "Direct horizontal irradiance [W/m²]",
+    datetime.strptime("2017-04-18", STRANG_DATE_FORMAT),
+    STRANG_GET_NOW,
 )
 STRANG_PARAMETERS[122] = STRANG(
     122,
     "Diffuse irradiance [W/m²]",
     datetime.strptime("2017-04-18", STRANG_DATE_FORMAT),
-    get_now,
+    STRANG_GET_NOW,
 )
