da9a2d9606f72c25daf9c246053c4042e3701a0e
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-09-30 20:56:43 +0200

    Remove redundant contidtion

--- test/unit/test_unit_metobs.py
+++ test/unit/test_unit_metobs.py
@@ -188,8 +188,6 @@ class TestUnitMetObsParameterV1:
             data_type: format of api data
             expected_type: expected result
         """
-        if data_type is None:
-            client = MetObs()
         if data_type != "json":
             with pytest.raises(NotImplementedError):
                 MetObs("yaml")
