94c0daabb634cc6d721094d9db07cd2638c0431c
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-10-01 11:14:33 +0200

    Fix flake8

--- smhi/constants.py
+++ smhi/constants.py
@@ -6,3 +6,9 @@ from collections import defaultdict
 
 METOBS_URL = "https://opendata-download-metobs.smhi.se/api.json"
 TYPE_MAP = defaultdict(lambda: "application/json", json="application/json")
+METOBS_AVAILABLE_PERIODS = [
+    "latest-hour",
+    "latest-day",
+    "latest-months",
+    "corrected-archive",
+]
