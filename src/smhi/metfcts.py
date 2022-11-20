154e2695d885dcf033d0ed221f4302bda7d3e7e7
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-11-20 16:17:10 +0100

    Add metfcts class

---
+++ src/smhi/metfcts.py
@@ -0,0 +1,27 @@
+"""
+SMHI Metfcts API module.
+"""
+from smhi.constants import METFCTS_URL
+
+
+class Metfcts:
+    """
+    SMHI Metfcts module
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialise Metfcts.
+        """
+        self._category = "pmp3g"
+        self._version = 2
+
+        self.latitude = None
+        self.longitude = None
+        self.status = None
+        self.header = None
+        self.data = None
+        self.base_url = METFCTS_URL.format(
+            category=self._category, version=self._version
+        )
+        self.url = None
