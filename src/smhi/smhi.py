f38f000a86838338018a5531a07d77864ec08ab2
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-11-20 20:51:50 +0100

    Fix: lint in smhi.py

--- src/smhi/smhi.py
+++ src/smhi/smhi.py
@@ -1,7 +1,6 @@
 """
 Read SMHI data.
 """
-from typing import Union
 from geopy import distance
 from smhi.metobs import Metobs
 from smhi.constants import TYPE_MAP
@@ -13,25 +12,60 @@ class SMHI:
     """
 
     def __init__(self, type: str = "json", version: str = "1.0"):
+        """Initialise SMHI class.
+
+        Args:
+            type: API type
+            version: API version
+        """
         self.type = TYPE_MAP[type]
         self.client = Metobs(type)
         self.client.fetch_parameters(version)
 
     @property
     def parameters(self):
+        """Get available parameters.
+
+        Returns:
+            parameters
+        """
         return self.client.parameter.data
 
     def get_stations(self, parameter: str = None):
+        """Get stations from parameter.
+
+        Args:
+            parameter: station parameter
+
+        Returns:
+            stations
+        """
         self.client.fetch_stations(parameter)
         return self.client.station.data
 
-    def get_stations_from_title(self, parameter: str = None):
-        self.client.fetch_stations(None, parameter)
+    def get_stations_from_title(self, title: str = None):
+        """Get stations from title.
+
+        Args:
+            title: station title
+
+        Returns:
+            stations
+        """
+        self.client.fetch_stations(None, title)
         return self.client.station.data
 
     def find_stations_from_gps(
         self, parameter: int, dist: float, latitude: float, longitude: float
     ):
+        """Find stations for parameter from gps location.
+
+        Args:
+            parameter: station parameter
+            dist: distance from gps location
+            latitude: latitude
+            longitude: longitude
+        """
         user_position = (latitude, longitude)
         self.get_stations(parameter)
         self.d = []
