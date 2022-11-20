685188c84c6fe5c7b404fc5f80d5ef4967c4c6fb
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-11-20 21:14:50 +0100

    Fix: lint in text_unit_smhi.py

--- tests/unit/test_unit_smhi.py
+++ tests/unit/test_unit_smhi.py
@@ -1,20 +1,14 @@
-"""
-SMHI unit tests.
-"""
-
+"""SMHI unit tests."""
 from unittest.mock import patch
 from smhi.smhi import SMHI
 
 
 class TestUnitSMHI:
-    """
-    Unit tests for SMHI class.
-    """
+    """Unit tests for SMHI class."""
 
     @patch("smhi.smhi.Metobs")
     def test_unit_smhi_init(self, mock_requests_metobs):
-        """
-        Unit test for SMHI init method.
+        """Unit test for SMHI init method.
 
         Args:
             mock_requests_metobs: mock MetObs object
