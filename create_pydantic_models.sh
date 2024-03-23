fb6885b8ce09efa314c9fc53af172a5183bd44fc
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2024-03-23 20:09:51 +0100

    Add script generating metobs pydantic models from url

---
+++ create_pydantic_models.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+datamodel-codegen --output-model-type pydantic_v2.BaseModel --url https://opendata-download-metobs.smhi.se/api.json --class-name VersionModel --output src/smhi/models/metobs_versions.py
+datamodel-codegen --output-model-type pydantic_v2.BaseModel --url https://opendata-download-metobs.smhi.se/api/version/1.0.json --class-name ParameterModel --output src/smhi/models/metobs_parameters.py
+datamodel-codegen --output-model-type pydantic_v2.BaseModel --url https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/1.json --class-name StationModel --output src/smhi/models/metobs_stations.py
+datamodel-codegen --output-model-type pydantic_v2.BaseModel --url https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/1/station/1.json --class-name PeriodModel --output src/smhi/models/metobs_periods.py
+datamodel-codegen --output-model-type pydantic_v2.BaseModel --url https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/1/station/1/period/corrected-archive.json --class-name DataModel --output src/smhi/models/metobs_data.py
