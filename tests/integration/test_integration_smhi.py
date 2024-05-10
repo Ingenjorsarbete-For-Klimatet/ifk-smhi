52f64ad2bb46b3843134e4034d4cea8f119247dd
Anders Nord <dr.anders.nord@gmail.com>
2024-05-10 05:56:57 +0200

    Initial integration test status

--- tests/integration/test_integration_smhi.py
+++ tests/integration/test_integration_smhi.py
@@ -1,31 +1,94 @@
 """SMHI integration tests."""
 
+from unittest.mock import patch
+
+import pandas as pd
+import pytest
+from smhi.smhi import SMHI
+
+
+class MockMetobsStationLink:
+    """Support class to mock MetObs StationLink object."""
+
+    def __init__(self, id: int, name: str, lat: float, lon: float):
+        """Initiate parameters."""
+        self.id = id
+        self.name = name
+        self.latitude = lat
+        self.longitude = lon
+
+
+class MockParameterModel:
+    """Support class to mock Metobs Parameter object."""
+
+    def __init__(self, station: MockMetobsStationLink):
+        """Initiate station."""
+        self.station = station
+
+
+class MockDataModel:
+    """Support class to mock Metobs Data object."""
+
+    def __init__(self, station: MockMetobsStationLink, parameter: MockParameterModel):
+        """initiate data."""
+        df = pd.DataFrame(
+            {
+                "Lufttemperatur": [19.0, 18.2, 15.3, 17.0, 18.8],
+                "Kvalitet": ["G", "G", "G", "G", "G"],
+            },
+            index=pd.to_datetime(
+                [
+                    "1859-08-01 07:00:00+00:00",
+                    "1859-08-01 13:00:00+00:00",
+                    "1859-08-01 20:00:00+00:00",
+                    "1859-08-02 07:00:00+00:00",
+                    "1859-08-02 13:00:00+00:00",
+                ],
+            ),
+        )
+        self.df = df
+
 
 class TestIntegrationSMHI:
     """Integration test of SMHI class."""
 
-
-   @pytest.mark.parametrize(
-        "parameter, station, period, parameter_data, station_data, period_data, data_title, data_df",
+    @pytest.mark.parametrize(
+        "parameter, city, period,radius,expected_result",
         [
             (
                 1,
-                1,
-                "corrected-archive",
-                MetobsVersionItem(
-                    key="1",
-                    title="Lufttemperatur",
-                    summary="momentanvärde, 1 gång/tim",
-                    unit="celsius",
-                ),
-                (1, "Akalla"),
-                "corrected-archive",
-                "Lufttemperatur - Akalla - Kvalitetskontrollerade historiska "
-                + "data (utom de senaste 3 mån): Ladda ner data",
-                {},
+                "Göteborg",
+                None,
+                None,
+                "Result",
             ),
         ],
     )
-    def test_integration_smhi(self):
+    @patch(
+        "smhi.metobs.Stations.__new__",
+        return_value=MockParameterModel([MockMetobsStationLink(1, "Göteborg", 16, 57)]),
+    )
+    @patch("smhi.metobs.Periods.__new__")
+    @patch(
+        "smhi.metobs.Data.__new__",
+        return_value=MockDataModel(
+            MockMetobsStationLink(1, "Göteborg", 16, 57),
+            MockParameterModel(MockMetobsStationLink(1, "Göteborg", 16, 57)),
+        ),
+    )
+    def test_integration_smhi(
+        self,
+        mock_data,
+        mock_periods,
+        mock_stations,
+        parameter,
+        city,
+        period,
+        radius,
+        expected_result,
+    ):
         """Integration test of SMHI class."""
-        
+
+        client = SMHI()
+        data = client.get_data_by_city(parameter, city)
+        assert len(data.df["Lufttemperatur"]) == 5
